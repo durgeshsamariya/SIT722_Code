215eba7f0d3401d0b20386015197388daf681091
Durgesh Samariya <samariya.durgesh@gmail.com>
2024-06-28 21:20:49 +1000

    Week 1 Tasks added.

--- week_01/example-3/main.py
+++ week_01/example-3/main.py
@@ -19,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=int(PORT), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(PORT), reload=True)
